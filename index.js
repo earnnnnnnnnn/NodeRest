f5a0165c4cbbf8853a402c3ba391b664671c5ac3
eannnnnnnnn <s6606021631017@email.kmutnb.ac.th>
2025-02-19 15:17:34 +0700

    commit

--- index.js
+++ index.js
@@ -67,5 +67,5 @@ app.delete('/books/:id', async (req, res) => {
 
 
 app.listen(5000, () => {
-  console.log('API server is listening on port 3000');
+  console.log('API server is listening on port 5000');
 });
